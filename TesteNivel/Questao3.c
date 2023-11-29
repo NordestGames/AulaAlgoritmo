975b53e2e518fe1c142848d381f11a9524a3a193
Anailton Fernandes <125313371+NordestGames@users.noreply.github.com>
2023-11-29 15:04:57 +0000

    Adicionado a questão 3 do teste

---
+++ TesteNivel/Questao3.c
@@ -0,0 +1,31 @@
+# include < stdio .h >
+
+int main ( ) {
+int N_ant , N_prox , N , N_atual
+N_ant = N_atual =1;
+for( i =1 ,i <= N , i ++) {
+N_prox = N_ant + N_atual ;
+N_ant = N_atual ;
+N_atual = N_prox ;
+printf ("Fim do laco !\n") ;
+return
+}
+
+// Erros que o codigo tem são:
+// 1- Falta ponto e vírgula(;) na declaração de variavel N_atual e também no return
+// 2- Variavel i, não foi declarada
+// 3- No for, as condições são separadas com ponto e vírgula(;)
+
+//   Codigo com as correções 
+# include <stdio.h>
+
+int main () {
+int N_ant , N_prox , N , N_atual;
+N_ant = N_atual =1;
+for(i =1 ;i <= N ;i ++) {
+N_prox = N_ant + N_atual;
+N_ant = N_atual;
+N_atual = N_prox;
+printf ("Fim do laco !\n");
+return 0;
+}
