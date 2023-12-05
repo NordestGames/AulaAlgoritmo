cd38708f7cab21f09287b91df1cd24059b8eadfe
Anailton Fernandes <125313371+NordestGames@users.noreply.github.com>
2023-12-05 20:06:35 +0000

    locação

--- LocaçãoDinamica/locacao.c
+++ LocaçãoDinamica/locacao.c
@@ -4,9 +4,34 @@
 int main(){
 int *vetor = malloc(3 * sizeof(int));
 if (vetor == NULL){
-    pintf("Erro na loca~cão de memoria");
+    printf("Erro na loca~cão de memoria");
     exit(1);
 }
+free(vetor);
+    return 0;
+}
+
+
+#include <stdio.h> // printf, scanf
+#include <stdlib.h> // Função malloc, calloc, free, exit
+
+int main(void){
+    int tamanho = 5;
+    int i;
+
+    int *vetor = (int*)malloc(3 * sizeof(int));
+    if (vetor == NULL){
+        printf("Erro na locacão de memoria");
+        exit(1); //Aborta a execução do programa
+}
+    else{
+        printf("Alocacao realizada com sucesso!\n ");
+    }
+// Ler os dados do vetor 
+    printf("Digite o valor do vetor:\n");
+    for(i = 0; i < tamanho; i++){
+        scanf("%d", &vetor[i]);
+    }
 free(vetor);
     return 0;
 }
